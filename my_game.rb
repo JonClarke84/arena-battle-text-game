8fad7e25f11403770fb874e48db380f0fc721749
Jon Clarke <jonclarke1984@googlemail.com>
2022-02-25 23:42:27 +0000

    Tweaking combat engine, still very much a wip. Special attacks don't yet work

--- my_game.rb
+++ my_game.rb
@@ -14,7 +14,12 @@ class Character
   end
 
   def attack
-    return @attack_damage
+    crit = rand(1..6)
+    if crit >= 5
+      return @attack_damage * 2
+    else
+      return @attack_damage
+    end
   end
 
   def take_damage(attack_damage)
@@ -23,11 +28,19 @@ class Character
 end
 
 def init
-  puts "Welcome to the arena!"
+  puts "*************************************************"
+  puts "***********WELCOME TO THE ARENA!*****************"
+  puts "***************GET READY!************************"
+  puts ""
+  print "Enter your name and press ENTER: "
 end
 
 def create_player
   player = Character.new(gets.chomp, 100, 20)
+end
+
+def create_enemy(name, health, damage)
+  enemy = Character.new(name, health, damage)
 end
 
 def key_check(desired_keys, input)
@@ -37,46 +50,56 @@ def key_check(desired_keys, input)
   end
 end
 
-init
-print "Enter your name and press ENTER: "
-player = create_player
+def init_combat(enemy, player)
+  puts ""
+  puts "*************************************************"
+  puts "TODAY YOU WILL BE FIGHTING #{enemy.name.upcase}!"
+  puts "***************GET READY!************************"
+  puts ""
 
-puts ""
-puts "*************************************************"
-puts "TODAY YOU WILL BE FIGHTING THE UNASSUMING GOBLIN!"
-puts "***************GET READY!************************"
-puts ""
+  while enemy.health > 0 && player.health > 0
+    puts "*************************************************#{player.name}: #{player.health}HP"
+    puts "*************************************************#{enemy.name}: #{enemy.health}HP"
+    puts ""
+    puts "A for attack, D for dodge, S for special, H for health potion"
 
-goblin = Character.new("The Unassuming Goblin", 75, 15)
+    print "Choose your action and press ENTER: "
+    input =  gets.chomp.downcase
+    
+    desired_keys = "a"
 
-while goblin.health > 0 && player.health > 0
+    key_check(desired_keys, input)
+
+    enemy.take_damage(player.attack)
+
+    puts "\e[H\e[2J"
+
+    puts "You hit #{enemy.name} for #{player.attack} damage!"
+    puts ""
+
+    player.take_damage(enemy.attack)
+    puts "#{enemy.name} hits you back for #{enemy.attack} damage!"
+    puts ""
+
+  end
+
   puts "*************************************************#{player.name}: #{player.health}HP"
-  puts "*************************************************#{goblin.name}: #{goblin.health}HP"
-  puts ""
-  puts "A for attack, D for dodge, S for special, H for health potion"
-
-  print "Choose your action and press ENTER: "
-  input =  gets.chomp.downcase
-  
-  desired_keys = "a"
-
-  key_check(desired_keys, input)
-
-  goblin.take_damage(player.attack)
-
-  puts "\e[H\e[2J"
-
-  puts "You hit #{goblin.name} for #{player.attack} damage!"
+  puts "*************************************************#{enemy.name}: #{enemy.health}HP"
   puts ""
 
-  player.take_damage(goblin.attack)
-  puts "#{goblin.name} hits you back for #{goblin.attack} damage!"
-  puts ""
+  victory_condition(player.health, enemy.health)
 
 end
 
-if player.health > goblin.health
-  puts "CONGRATULATIONS, YOU WIN!"
-else
-  puts "YOU LOSE!"
+def victory_condition(player_health, enemy_health)
+  if player_health > enemy_health
+    puts "CONGRATULATIONS, YOU WIN!"
+  else
+    puts "YOU LOSE!"
+  end
 end
+
+init
+player = create_player
+goblin = create_enemy("The Fat Goblin", 80, 15)
+init_combat(goblin, player)
